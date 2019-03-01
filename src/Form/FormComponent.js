7492dccb686cf310436672cb6ff72f55536923dc
leianne louis <leilouis112@gmail.com>
2019-03-01 12:32:53 -0500

    completed boiler plate setup

---
+++ src/Form/FormComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const FormComponent = props => {
+    return(
+        <h1>Connected</h1>
+    )
+}
+
+export default FormComponent;
---
+++ src/Form/FormView.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react';
+import FormComponent from './FormComponent';
+
+class FormView extends Component {
+    render(){
+        return(
+            <FormComponent />
+        )
+    }
+}
+
+export default FormView
